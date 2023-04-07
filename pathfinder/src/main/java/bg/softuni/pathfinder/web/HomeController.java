100406da472935bd63762c759a89d54ab468f7cd
My PC <alex.petkov606@gmail.com>
2023-04-07 14:55:35 +0300

    asd

--- pathfinder/src/main/java/bg/softuni/pathfinder/web/HomeController.java
+++ pathfinder/src/main/java/bg/softuni/pathfinder/web/HomeController.java
@@ -1,6 +1,6 @@
 package bg.softuni.pathfinder.web;
 
-import bg.softuni.pathfinder.model.Picture;
+
 import bg.softuni.pathfinder.model.Route;
 import bg.softuni.pathfinder.service.RouteService;
 import org.springframework.stereotype.Controller;
@@ -8,7 +8,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
-import java.util.Optional;
 
 @Controller
 public class HomeController {
