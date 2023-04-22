9e01b3e3208c6fe752b13435b79abb2fc12f0dd5
My PC <alex.petkov606@gmail.com>
2023-04-22 12:34:32 +0300

    asd

--- pathfinder/src/main/java/bg/softuni/pathfinder/config/PathfinderSecurityConfiguration.java
+++ pathfinder/src/main/java/bg/softuni/pathfinder/config/PathfinderSecurityConfiguration.java
@@ -1,6 +1,7 @@
 package bg.softuni.pathfinder.config;
 
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,7 +13,21 @@ public class PathfinderSecurityConfiguration {
 
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeRequests()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                .antMatchers("/", "/users/login", "/users/register").permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/users/login")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .defaultSuccessUrl("/")
+                .failureForwardUrl("/users/login?error=true");
+
+        return http.build();
 
     }
 }
